757af42dcd3540a1668308cd502d4ba4a6e30c82
Daniel Okello <handsdani@gmail.com>
2025-03-08 17:57:55 +0300

    git init

---
+++ template.py
@@ -0,0 +1,35 @@
+import os
+from pathlib import Path
+import logging
+
+#  Logging stream
+
+logging.basicConfig(level=logging.INFO,format='[%(ascitime)s]: %(message)s')
+
+files = [
+    "src/__init__.py",
+    "src/helper.py",
+    ".env",
+    "requirements.txt",
+    "setup.py",
+    "app.py",
+    "data",
+    "research/trials.py"
+]
+
+
+for filepath in files:
+    filepath = Path(filepath)
+    filedir,filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"creating directory: {filedir} for file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filename) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"creating empty file: {filepath}")
+
+    else:
+        logging.info(f"{filename} is already exists")
